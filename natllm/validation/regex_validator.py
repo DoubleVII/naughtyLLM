640271b802f12fdf6e77ef7be02b28e22d6b2b27
王旖 <1226007719@qq.com>
2024-07-22 22:29:09 +0800

    Update regex_validator.py with FSM integration

--- natllm/validation/regex_validator.py
+++ natllm/validation/regex_validator.py
@@ -34,16 +34,20 @@ class RegexValidator(Validator):
         # print(self.fsm.states)
         # print(self.fsm.finals)
         self.current_state = None
+        self.last_state = None
 
     def init_state(self) -> None:
         if hasattr(self.fsm, 'initial'):
-            self.current_state = self.fsm.initial
+            self.last_state = self.fsm.initial
         else:
             raise AttributeError("FSM 对象没有 initial 属性作为起始状态")
 
     def validate(self, input_str: str) -> bool:
         print(f"Validating input: {input_str}")
-        self.current_state = self.fsm.initial
+        # 若上一个test_token 验证不通过，则 self.last_state未更新，即仍为上一个test_token的初始状态
+        # 若上一个test_token 验证通过，则 self.last_state更新，即为上一个test_token的最终状态
+        self.current_state = self.last_state
+
         for char in input_str:
             # 获取当前字符的 TransitionKey
             # 如果 char 在 self.fsm.alphabet 中，获取其转移键
@@ -55,13 +59,23 @@ class RegexValidator(Validator):
                 transition_key = self.fsm.alphabet.get("anything_else", None)
 
             # print(transition_key)
+            print(f"Current state: {self.current_state}, Character: {char}, Transition key: {transition_key}")
 
+            # 若当前状态是状态转移表中的一个状态；
+            # 并且在当前状态下，当前字符在状态转移字典中有定义相应的状态转移，则更新当前状态为转移后的状态
             if self.current_state in self.fsm.map and transition_key in self.fsm.map[self.current_state]:
                 self.current_state = self.fsm.map[self.current_state][transition_key]
+            # 否则当前状态不接收此字符，本次test_token验证不通过，不更新self.last_state，使之仍为本次test_token的初始状态
             else:
                 print(f"Character '{char}' not in FSM transitions for state {self.current_state}")
                 return False
-        return self.current_state in self.fsm.finals
+
+        # 本次test_token验证通过，更新self.last_state为本次test_token的最终状态
+        self.last_state = self.current_state
+        print(f"Final state: {self.current_state}")
+        return True
 
     def finish(self) -> bool:
         return self.current_state in self.fsm.finals
+
+
