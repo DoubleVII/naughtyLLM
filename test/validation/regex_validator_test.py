a9d85b1e81599bfcd07dc23c139817a217c32b04
yangsen.double7 <yangsen.double7@bytedance.com>
2024-07-22 15:08:16 +0800

    add new test for validation

--- test/validation/regex_validator_test.py
+++ test/validation/regex_validator_test.py
@@ -56,3 +56,34 @@ def test_regexValidator_expression():
     for expr in invalid_expressions:
         assert not validator.validate(expr), f"Invalid expression passed: {expr}"
         assert not validator.finish(), f"Invalid expression finished correctly: {expr}"
+
+
+def test_regexValidator_basic2():
+    regex = "genshin impact"
+
+    test_tokens = ["gen", "shin ", "impact"]
+    validator = RegexValidator(regex)
+
+    validator.init_state()
+
+    for test_token in test_tokens:
+        assert validator.validate(test_token)
+
+    assert validator.finish()
+
+
+def test_regexValidator_recover():
+    regex = "123456789"
+
+    test_tokens = ["123", "456", "780", "789"]
+    validator = RegexValidator(regex)
+
+    validator.init_state()
+
+
+    assert validator.validate(test_tokens[0])
+    assert validator.validate(test_tokens[1])
+    assert not validator.validate(test_tokens[2])
+    assert validator.validate(test_tokens[3])
+
+    assert validator.finish()
